80fbc048418ab048cd5c97cae27de64f2e32c7fb
bbzdevpr <bednarz.prof@gmail.com>
2024-12-31 20:09:33 +0100

    url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -100,7 +100,7 @@ const config = {
           },
           //{to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://bbzdevpr.github.io/CIEL-docusaurus',
             label: 'GitHub',
             position: 'right',
           },
