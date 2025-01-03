efe885f22aeb052d9897a9cd6849c010f950b85b
bbzdevpr <bednarz.prof@gmail.com>
2025-01-03 22:48:13 +0100

    modif de la page d'accueil

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Cybersécurité',
+    Svg: require('@site/static/img/cybersecurite.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Apprendre à protéger ses données.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Informatique et réseau',
+    Svg: require('@site/static/img/informatique et reseau.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        L'informatique et le réseau, voyons les bases
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Electronique',
+    Svg: require('@site/static/img/electronique.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Electronique, reprenons le controle
       </>
     ),
   },
